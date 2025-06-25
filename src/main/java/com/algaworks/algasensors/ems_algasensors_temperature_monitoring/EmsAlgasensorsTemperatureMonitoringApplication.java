ea6aee5ada8168d35c9e372eae0f23690304bcbe
mailsonfp <mailsonfp.dev@gmail.com>
2025-06-24 23:33:39 -0300

    adicionando depenêndencias e arquivos estruturais

---
+++ src/main/java/com/algaworks/algasensors/ems_algasensors_temperature_monitoring/EmsAlgasensorsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_algasensors_temperature_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsAlgasensorsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsAlgasensorsTemperatureMonitoringApplication.class, args);
+	}
+
+}
